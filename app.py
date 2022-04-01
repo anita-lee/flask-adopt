ae4d16cd9ec984aa1da8e6147b29c0d01e950dd5
calebthewood <calebwood.cs@gmail.com>
2022-04-01 15:57:34 -0700

    Adds Pet and Add Pet Form models

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 """Flask app for adopt app."""
 
-from flask import Flask, render_template
+from smtplib import SMTPRecipientsRefused
+from flask import Flask, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
+from forms import AddPetForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -25,4 +27,33 @@ def list_pets():
     pets = Pet.query.all()
 
     return render_template ("homepage.html", pets=pets)
+
+@app.route("/add", methods=["GET", "POST"])
+def add_pet():
+    """Add pet form. Handle Adding pet"""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        pet = Pet(name=name,
+                species=species,
+                photo_url=photo_url,
+                age=age, notes=notes)
+
+        db.session.add(pet)
+        db.session.commit()
+
+        flash(f" Added {name} to adoption list.")
+        return redirect("/add")
+
+    else:
+        return render_template("pet_add_form.html", form=form)
+
+
 
--- forms.py
+++ forms.py
@@ -1,1 +1,35 @@
 """Forms for adopt app."""
+from lib2to3.pgen2.token import OP
+from wsgiref.validate import validator
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField
+from wtforms.validators import InputRequired, Optional, URL
+
+class AddPetForm(FlaskForm):
+    """Form for adding snacks."""
+
+    name = StringField("Pet's Name",
+                        validators = [
+                            InputRequired()
+                        ])
+    species = StringField("Pet's Species",
+                        validators = [
+                            InputRequired()
+                        ])
+    photo_url = StringField('Add a link to image of pet',
+                        validators = [
+                            Optional(),
+                            URL()
+                        ])
+    age = SelectField("Select Pets Age Category",
+                        choices=[
+                            ('baby', 'Baby'),
+                            ('young', 'Young'),
+                            ('adult', 'Adult'),
+                            ('senior', 'Senior')
+                            ],
+                        validators = [
+                            InputRequired(),
+                        ])
+    notes = TextAreaField("Notes on pet")
+
